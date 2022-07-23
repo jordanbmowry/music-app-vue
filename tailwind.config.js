30938f3435eeb110bb50ecd01a543a926bf447cf
Jordan Mowry <jordan.mowry@gmail.com>
2022-07-23 11:43:21 -0500

    feat: split auth and header into components

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
